b0b1421dc466c37b2c71c913de7eb1f1bc8479ca
IvanFestina <ivanfestina@gmail.com>
2022-07-14 21:43:27 +0300

    progressing...

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 const express = require('express')
 const nodeMailer = require('nodemailer')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
+
 app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json)
+
+let smtp_login = process.env.SMPT_LOGIN || '---'
+let smtp_password = process.env.SMPT_LOGIN || '---'
 
 let transporter = nodeMailer.createTransport({
         tls: {
@@ -20,17 +27,24 @@ let transporter = nodeMailer.createTransport({
 app.get('/', function (req, res) {
     res.send('Hello World');
 })
-app.post('/sendMessage',  function (req, res) {
-    transporter.sendMail({
-    from: "Ivan Account",
+app.post('/sendMessage',  async function (req, res) {
+    let {message, contacts, name } = req.body;
+
+    let info = await transporter.sendMail({
+    from: "From my portfolio page",
     to: "ivanfestina@gmail.com",
-    subject: "Hello form node.js",
-    html: `<div>Hi from myself</div>`
+    subject: "From my portfolio page",
+    html: `<div>Message from your portfolio page</div>
+            <div>${message}</div>
+            <div>${contacts}</div>
+            <div>${name}</div>`
     });
-    res.send('something happened!');
+    res.send('ok');
 });
 
-app.listen(3010, function () {
+let port = process.env.PORT || 3010;
+
+app.listen(port, function () {
     console.log('Example app listening on port 3010')
 });
 
